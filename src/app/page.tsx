682b04ef57bda95d1313a081c25f2fc90d61bd8e
shyam <shyamjith38@hotmail.com>
2024-09-01 14:20:24 +0530

    learned link component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function Home(){
-    return <h1>Welcome Home</h1>;
+    return (
+    <>
+    <h1>Welcome Home</h1>
+    
+    <Link href='/blog'>Blog</Link>
+    <Link href='/products'>products</Link>
+
+    </>);
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link';
 export default function ProductList(){
+    const productId=100;
     return (
         <>
+        <Link href='/'>HOME</Link>
         <h1>Product List</h1>
-        <h2>product 1</h2>
-        <h2>product 2</h2>
-        <h2>product 3</h2>
+        <h2><Link href="products/1">product 1</Link></h2>
+        <h2><Link href="products/2">product 2</Link></h2>
+        <h2><Link href="products/3" replace>product 3</Link></h2>
+        <h2><Link href={`products/${productId}`}>product {productId}</Link></h2>
         </>
     );
 }
