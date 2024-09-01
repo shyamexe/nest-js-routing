2f3fbf1a16f579bb076319d3d1f1b2310817378a
shyam <shyamjith38@hotmail.com>
2024-09-01 13:53:54 +0530

    learned routing metadata

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,9 @@
+export const metadata ={
+    title: "About Shyam",
+    
+}
+
+
 export default function About(){
     return <h1>About</h1>;
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,24 @@
-export default function ProductDetails({ params }: { params: { productId: string } }) {
+import { Metadata } from "next";
+
+type Props ={
+    params:{
+        productId: string;
+    };
+};
+
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+    const title = await new Promise(resolve =>{
+        setTimeout(()=> resolve(`iPhone ${params.productId}`,),500)
+    })
+    return {
+        title: `Product ${title}`,
+    };
+};
+
+
+
+
+export default function ProductDetails({ params }: Props ) {
 
     return <h1>Deatails about Product {params.productId} </h1>
 }
